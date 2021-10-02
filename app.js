b75663eb64992a0182680445f21c00148eb26c41
aman kushwaha <amankushwaha933@gmail.com>
2021-10-02 22:29:24 +0530

    fixed input bug

--- app.js
+++ app.js
@@ -27,8 +27,8 @@ reset.addEventListener('click', function () {
 		ul.children[k].textContent = `Task-${k + 1}`;
 		ul.children[k].style.fontSize = '.8rem';
 		ul.children[k].style.color = 'hsl(1, 10%, 50%)';
-		i = 0;
 	}
+	i = 0;
 });
 
 // ul.children[0].textContent = 'aman';
