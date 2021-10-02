3ad448dd7b445307df7b7352ffe06553c3bc18ee
aman kushwaha <amankushwaha933@gmail.com>
2021-10-02 22:23:27 +0530

    fixed reset

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const submit = document.querySelector('.submit');
 const input = document.querySelector('.input');
 const ul = document.querySelector('ul');
+const reset = document.querySelector('.reset');
+
 let str = '';
 let j = ul.children.length;
 console.log(j);
@@ -15,8 +17,17 @@ submit.addEventListener('click', function () {
 	ul.children[i].style.fontSize = '1rem';
 	ul.children[i].style.color = 'red';
 	i++;
+	input.value = '';
 
 	str = '';
+});
+
+reset.addEventListener('click', function () {
+	for (let k = 0; k < j; k++) {
+		ul.children[k].textContent = `Task-${k + 1}`;
+		ul.children[k].style.fontSize = '.8rem';
+		ul.children[k].style.color = 'hsl(1, 10%, 50%)';
+	}
 });
 
 // ul.children[0].textContent = 'aman';
